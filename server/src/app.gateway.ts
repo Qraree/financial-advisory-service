6c2704e8336efa690f77d5043d023296d5f418f1
Qrare <Metbezu@gmail.com>
2022-11-27 18:00:24 +0300

    added web socket server side

--- server/src/app.gateway.ts
+++ server/src/app.gateway.ts
@@ -5,9 +5,19 @@ import { SubscribeMessage,
   OnGatewayDisconnect,
   WebSocketServer
 } from '@nestjs/websockets';
+import {Chat} from "./chat/chat.model";
+import {AppService} from "./app.service";
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+
+
 export class AppGateway {
+  constructor(private appService: AppService) {
+  }
 
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
